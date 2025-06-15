c8e76bcd959f27592aeebd808c835bc7ce566888
cabetoDLP <cabetodp@gmail.com>
2025-06-14 19:54:32 -0500

    Bazaar app project uploaded

--- scripts.js
+++ scripts.js
@@ -264,6 +264,8 @@ document.getElementById(`spring`).addEventListener(`click`, function() {openLink
 document.getElementById(`reactStore`).addEventListener(`click`, function() {openLink(`https://orange-grass-05a93b810.6.azurestaticapps.net/login`)});
 document.getElementById(`cabetoArt`).addEventListener(`click`, function() {openLink(`https://cabeto-art.vercel.app/`)});
 document.getElementById(`dinoLink`).addEventListener(`click`, function() {openLink(`https://dino-link.vercel.app/`)});
+document.getElementById(`bazaarApp`).addEventListener(`click`, function() {openLink(`https://bazaar-app-frontend.vercel.app/`)});
+
 
 document.querySelector(`.contact-form`).addEventListener(`submit`, function (event){
 
